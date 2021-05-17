9cf1d18e42bd49cad39acb7905af01a17945d6d9
Chris Lopez <chrislopez28@gmail.com>
2021-05-17 06:53:35 -0700

    Added Backdrop to SideDrawer component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import { useState, useEffect, useRef } from 'react';
-import { connect } from 'react-redux';
+import { useState, useEffect, useRef } from "react";
+import { connect } from "react-redux";
 
-import classes from './App.module.css';
-import { arrayEquals, shuffle } from './util/array';
-import { encodeGame } from './util/gridEncoding';
-import useInterval from './util/useInterval';
-import * as actions from './store/actions/session';
+import classes from "./App.module.css";
+import { arrayEquals, shuffle } from "./util/array";
+import { encodeGame } from "./util/gridEncoding";
+import useInterval from "./util/useInterval";
+import * as actions from "./store/actions/session";
 
-import Button from './components/Button/Button';
-import ControlButton from './components/Button/ControlButton';
-import DrawerToggle from './components/SideDrawer/DrawerToggle/DrawerToggle';
-import Grid from './components/Grid/Grid';
-import Modal from './components/UI/Modal/Modal';
-import SideDrawer from './components/SideDrawer/SideDrawer';
+import Button from "./components/Button/Button";
+import ControlButton from "./components/Button/ControlButton";
+import DrawerToggle from "./components/SideDrawer/DrawerToggle/DrawerToggle";
+import Grid from "./components/Grid/Grid";
+import Modal from "./components/UI/Modal/Modal";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
 
 function App(props) {
-
   // State
   const [game, setGame] = useState({
     matrix: [0, 0, 0, 0, 0, 0, 0, 0, 0],
     target: [0, 0, 0, 0, 0, 0, 0, 0, 0],
     gameId: "",
     isSolved: null,
-    moveCount: 0
-  })
+    moveCount: 0,
+  });
 
   const gameRef = useRef(game);
 
@@ -33,12 +32,12 @@ function App(props) {
     isAutoplay: false,
     isStart: true,
     showMenu: false,
-    difficulty: 'normal',
+    difficulty: "normal",
     resetDelay: 500,
-    isFirstLoad: true
-  })
+    isFirstLoad: true,
+  });
 
-  const [movement, setMovement] = useState('')
+  const [movement, setMovement] = useState("");
 
   // Hooks
   useInterval(() => {
@@ -48,42 +47,47 @@ function App(props) {
       } else {
         setGameSettings({
           ...gameSettings,
-          isAutoplay: false
-        })
+          isAutoplay: false,
+        });
       }
     }
-  })
+  });
 
   // TODO: Keyhandlers
 
   const handleKeyDown = (e) => {
     if (gameRef.current.isSolved) {
-      return
+      return;
     }
-    if (e.key === 'w' || e.key === 'W' || e.keyCode === 38) {
-      slide('moveUp', true)
+    if (e.key === "w" || e.key === "W" || e.keyCode === 38) {
+      slide("moveUp", true);
     }
-    if (e.key === 's' || e.key === 'S' || e.keyCode === 40) slide('moveDown', true)
-    if (e.key === 'a' || e.key === 'A' || e.keyCode === 37) slide('moveLeft', true)
-    if (e.key === 'd' || e.key === 'D' || e.keyCode === 39) slide('moveRight', true)
-    if (e.key === 'q' || e.key === 'Q' || e.keyCode === 33) slide('rotateLeft', true)
-    if (e.key === 'e' || e.key === 'E' || e.keyCode === 34) slide('rotateRight', true)
-  }
+    if (e.key === "s" || e.key === "S" || e.keyCode === 40)
+      slide("moveDown", true);
+    if (e.key === "a" || e.key === "A" || e.keyCode === 37)
+      slide("moveLeft", true);
+    if (e.key === "d" || e.key === "D" || e.keyCode === 39)
+      slide("moveRight", true);
+    if (e.key === "q" || e.key === "Q" || e.keyCode === 33)
+      slide("rotateLeft", true);
+    if (e.key === "e" || e.key === "E" || e.keyCode === 34)
+      slide("rotateRight", true);
+  };
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown)
+      window.removeEventListener("keydown", handleKeyDown);
     };
-  })
+  });
 
   // Function Declarations
   function newGame(difficulty) {
     let numberColors = 1;
     let numberSquaresMax = 4;
 
-    if (difficulty === 'hard') {
+    if (difficulty === "hard") {
       numberColors = 2;
       numberSquaresMax = 5;
     }
@@ -95,7 +99,7 @@ function App(props) {
     const newMatrix = [];
     for (let i = 0; i < 9; i++) {
       if (numSquares < numSquaresMax) {
-        randomColor = Math.floor(Math.random() * (numberColors));
+        randomColor = Math.floor(Math.random() * numberColors);
 
         if (i < 2 && numberColors > 1) {
           if (i === 0) {
@@ -107,21 +111,21 @@ function App(props) {
         }
         switch (randomColor) {
           case 0:
-            newMatrix.push(1)
+            newMatrix.push(1);
             break;
           case 1:
-            newMatrix.push(2)
+            newMatrix.push(2);
             break;
           case 2:
-            newMatrix.push(3)
+            newMatrix.push(3);
             break;
           default:
-            newMatrix.push(0)
+            newMatrix.push(0);
             break;
         }
         numSquares += 1;
       } else {
-        newMatrix.push(0)
+        newMatrix.push(0);
       }
     }
 
@@ -139,12 +143,12 @@ function App(props) {
       target: updatedTarget,
       gameId: encodeGame(updatedMatrix, updatedTarget),
       isSolved: false,
-      moveCount: 0
-    }
+      moveCount: 0,
+    };
 
-    setGame(newGame)
+    setGame(newGame);
     gameRef.current = newGame;
-    console.log(gameRef.current)
+    console.log(gameRef.current);
   }
 
   function slide(moveType, referenceMatrix = false) {
@@ -156,43 +160,103 @@ function App(props) {
       target = gameRef.current.target;
     }
 
-    console.log(matrix)
+    console.log(matrix);
 
     switch (moveType) {
-      case 'rotateLeft':
-        matrix = [matrix[1], matrix[2], matrix[5], matrix[0], matrix[4], matrix[8], matrix[3], matrix[6], matrix[7]]
+      case "rotateLeft":
+        matrix = [
+          matrix[1],
+          matrix[2],
+          matrix[5],
+          matrix[0],
+          matrix[4],
+          matrix[8],
+          matrix[3],
+          matrix[6],
+          matrix[7],
+        ];
         break;
-      case 'rotateRight':
-        matrix = [matrix[3], matrix[0], matrix[1], matrix[6], matrix[4], matrix[2], matrix[7], matrix[8], matrix[5]]
+      case "rotateRight":
+        matrix = [
+          matrix[3],
+          matrix[0],
+          matrix[1],
+          matrix[6],
+          matrix[4],
+          matrix[2],
+          matrix[7],
+          matrix[8],
+          matrix[5],
+        ];
         break;
-      case 'moveUp':
-        matrix = [matrix[3], matrix[4], matrix[5], matrix[6], matrix[7], matrix[8], matrix[0], matrix[1], matrix[2]]
+      case "moveUp":
+        matrix = [
+          matrix[3],
+          matrix[4],
+          matrix[5],
+          matrix[6],
+          matrix[7],
+          matrix[8],
+          matrix[0],
+          matrix[1],
+          matrix[2],
+        ];
         break;
-      case 'moveDown':
-        matrix = [matrix[6], matrix[7], matrix[8], matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]]
+      case "moveDown":
+        matrix = [
+          matrix[6],
+          matrix[7],
+          matrix[8],
+          matrix[0],
+          matrix[1],
+          matrix[2],
+          matrix[3],
+          matrix[4],
+          matrix[5],
+        ];
         break;
-      case 'moveLeft':
-        matrix = [matrix[1], matrix[2], matrix[0], matrix[4], matrix[5], matrix[3], matrix[7], matrix[8], matrix[6]]
+      case "moveLeft":
+        matrix = [
+          matrix[1],
+          matrix[2],
+          matrix[0],
+          matrix[4],
+          matrix[5],
+          matrix[3],
+          matrix[7],
+          matrix[8],
+          matrix[6],
+        ];
         break;
-      case 'moveRight':
-        matrix = [matrix[2], matrix[0], matrix[1], matrix[5], matrix[3], matrix[4], matrix[8], matrix[6], matrix[7]]
+      case "moveRight":
+        matrix = [
+          matrix[2],
+          matrix[0],
+          matrix[1],
+          matrix[5],
+          matrix[3],
+          matrix[4],
+          matrix[8],
+          matrix[6],
+          matrix[7],
+        ];
         break;
       default:
         break;
     }
 
-    console.log(matrix, target)
-    updateMatrix(matrix, true)
+    console.log(matrix, target);
+    updateMatrix(matrix, true);
 
-    setMovement(moveType)
-    setTimeout(() => setMovement(''), 500);
+    setMovement(moveType);
+    setTimeout(() => setMovement(""), 500);
   }
 
   function updateMatrix(updatedMatrix, useGameRef = false) {
     let newMoveCount = game.moveCount + 1;
     let isSolved = false;
     let prevGame = game;
-    let target = game.target
+    let target = game.target;
 
     if (useGameRef) {
       newMoveCount = gameRef.current.moveCount + 1;
@@ -203,7 +267,7 @@ function App(props) {
     if (arrayEquals(updatedMatrix, target)) {
       isSolved = true;
       if (gameSettings.isAutoplay) {
-        props.incrementSkipped()
+        props.incrementSkipped();
       } else {
         props.incrementSolved();
       }
@@ -213,12 +277,11 @@ function App(props) {
       ...prevGame,
       matrix: updatedMatrix,
       isSolved,
-      moveCount: newMoveCount
-    }
+      moveCount: newMoveCount,
+    };
 
     setGame(updatedGame);
     gameRef.current = updatedGame;
-
   }
 
   function randomize() {
@@ -252,16 +315,16 @@ function App(props) {
     const isAutoplay = !gameSettings.isAutoplay;
     setGameSettings({
       ...gameSettings,
-      isAutoplay: isAutoplay
-    })
+      isAutoplay: isAutoplay,
+    });
   }
 
   function startGameHandler(difficulty) {
     props.initializeSession();
     props.setDifficulty(difficulty);
     setGameSettings({
-      ...gameSettings
-    })
+      ...gameSettings,
+    });
     newGame(difficulty);
     toggleStart();
     setShowSideDrawer(false);
@@ -271,14 +334,18 @@ function App(props) {
     let isStart = !gameSettings.isStart;
     setGameSettings({
       ...gameSettings,
-      isStart: isStart
-    })
+      isStart: isStart,
+    });
   }
 
   // Conditional JSX Elements
   let next = null;
   if (game.isSolved) {
-    next = <Button size="next" onClick={() => newGame(props.difficulty)}>Next &rarr;</Button>
+    next = (
+      <Button size="next" onClick={() => newGame(props.difficulty)}>
+        Next &rarr;
+      </Button>
+    );
   }
 
   // JSX
@@ -286,62 +353,116 @@ function App(props) {
     <div className={classes.App}>
       <Modal show={gameSettings.isStart} modalClosed={toggleStart}>
         <h2>Select Difficulty:</h2>
-        <Button size="menu" onClick={() => startGameHandler('normal')}>Normal</Button><br />
-        <Button size="menu" onClick={() => startGameHandler('hard')}>Hard</Button><br /><br />
+        <Button size="menu" onClick={() => startGameHandler("normal")}>
+          Normal
+        </Button>
+        <br />
+        <Button size="menu" onClick={() => startGameHandler("hard")}>
+          Hard
+        </Button>
+        <br />
+        <br />
         <button onClick={toggleStart}>Cancel</button>
       </Modal>
 
-      <SideDrawer open={showSideDrawer} click={() => setShowSideDrawer(!showSideDrawer)}>
+      <SideDrawer
+        open={showSideDrawer}
+        click={() => setShowSideDrawer(!showSideDrawer)}
+      >
         <h2>Menu</h2>
-        <Button size="menu" onClick={toggleStart}>New Game</Button>
-        <Button size="menu" onClick={() => {
-          props.incrementSkipped();
-          newGame(props.difficulty);
-        }}>Skip</Button>
-        <Button size="menu" onClick={toggleAutoplay} on={gameSettings.isAutoplay}>Autosolve</Button>
+        <Button size="menu" onClick={toggleStart}>
+          New Game
+        </Button>
+        <Button
+          size="menu"
+          onClick={() => {
+            props.incrementSkipped();
+            newGame(props.difficulty);
+          }}
+        >
+          Skip
+        </Button>
+        <Button
+          size="menu"
+          onClick={toggleAutoplay}
+          on={gameSettings.isAutoplay}
+        >
+          Autosolve
+        </Button>
       </SideDrawer>
 
       <div className={classes.Top}>
         <div className={classes.Title}>Rubik's Slide</div>
-        <p>Press the arrows to shift and rotate the squares so that the design matches the target.</p>
+        <p>
+          Press the arrows to shift and rotate the squares so that the design
+          matches the target.
+        </p>
         <DrawerToggle click={() => setShowSideDrawer(!showSideDrawer)} />
       </div>
 
       <div className={classes.GridContainer}>
         <div className={classes.Row}>
-          <ControlButton type="rotateLeft" onClick={() => slide('rotateLeft')} disabled={game.isSolved} />
-          <ControlButton type="moveUp" onClick={() => slide('moveUp')} disabled={game.isSolved} />
-          <ControlButton type="rotateRight" onClick={() => slide('rotateRight')} disabled={game.isSolved} />
+          <ControlButton
+            type="rotateLeft"
+            onClick={() => slide("rotateLeft")}
+            disabled={game.isSolved}
+          />
+          <ControlButton
+            type="moveUp"
+            onClick={() => slide("moveUp")}
+            disabled={game.isSolved}
+          />
+          <ControlButton
+            type="rotateRight"
+            onClick={() => slide("rotateRight")}
+            disabled={game.isSolved}
+          />
         </div>
         <div className={classes.CenterRow}>
-          <ControlButton type="moveLeft" onClick={() => slide('moveLeft')} disabled={game.isSolved} />
-          <Grid matrix={game.matrix} movement={movement} isSolved={game.isSolved} />
-          <ControlButton type="moveRight" onClick={() => slide('moveRight')} disabled={game.isSolved} />
+          <ControlButton
+            type="moveLeft"
+            onClick={() => slide("moveLeft")}
+            disabled={game.isSolved}
+          />
+          <Grid
+            matrix={game.matrix}
+            movement={movement}
+            isSolved={game.isSolved}
+          />
+          <ControlButton
+            type="moveRight"
+            onClick={() => slide("moveRight")}
+            disabled={game.isSolved}
+          />
         </div>
         <div className={classes.Row}>
-          <ControlButton type="moveDown" onClick={() => slide('moveDown')} disabled={game.isSolved} />
+          <ControlButton
+            type="moveDown"
+            onClick={() => slide("moveDown")}
+            disabled={game.isSolved}
+          />
         </div>
       </div>
 
-      <div className={classes.NextButton}>
-        {next}
-      </div>
-
+      <div className={classes.NextButton}>{next}</div>
 
       <div className={classes.ControlBar}>
         <div className={classes.Score}>
-          <div><strong>Solved</strong>: {props.numberSolved}</div>
-          <div><strong>Skipped</strong>: {props.numberSkipped}</div>
-          <div><strong>Moves Current Puzzle</strong>: {game.moveCount}</div>
-          {/* <div>Difficulty: {props.difficulty.charAt(0).toUpperCase() + props.difficulty.slice(1)}</div>
-          <div>Game ID: {game.gameId}</div> */}
+          <div>
+            <strong>Solved</strong>: {props.numberSolved}
+          </div>
+          <div>
+            <strong>Skipped</strong>: {props.numberSkipped}
+          </div>
+          <div>
+            <strong>Moves Current Puzzle</strong>: {game.moveCount}
+          </div>
         </div>
 
         <div className={classes.Panel}>
           <div className={classes.TargetTitle}>Target</div>
           <Grid matrix={gameRef.current.target} mini />
         </div>
-
       </div>
     </div>
   );
@@ -351,17 +472,17 @@ const mapStateToProps = (state) => {
   return {
     numberSolved: state.session.numberSolved,
     numberSkipped: state.session.numberSkipped,
-    difficulty: state.session.difficulty
-  }
-}
+    difficulty: state.session.difficulty,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     setDifficulty: (difficulty) => dispatch(actions.setDifficulty(difficulty)),
     incrementSkipped: () => dispatch(actions.incrementSkipped()),
     incrementSolved: () => dispatch(actions.incrementSolved()),
-    initializeSession: () => dispatch(actions.initializeSession())
-  }
-}
+    initializeSession: () => dispatch(actions.initializeSession()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import styles from './SideDrawer.module.css';
+import Backdrop from "../UI/Backdrop/Backdrop";
+import styles from "./SideDrawer.module.css";
 
 export default function SideDrawer(props) {
-    let attachedClasses = [styles.SideDrawer, styles.Close]
-    if (props.open) {
-        attachedClasses = [styles.SideDrawer, styles.Open]
-    }
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
 
-    return (
-        <React.Fragment>
-            <div></div>
-            <div className={attachedClasses.join(' ')} >
-                {props.children}
+  return (
+    <>
+      <Backdrop show={props.open} click={props.click} />
+      <div className={attachedClasses.join(" ")}>
+        {props.children}
 
-                <button
-                    className={styles.CloseButton} 
-                    onClick={props.click}>Close</button>
-            </div>
-        </React.Fragment>
-    )
+        <button className={styles.CloseButton} onClick={props.click}>
+          Close
+        </button>
+      </div>
+    </>
+  );
 }
