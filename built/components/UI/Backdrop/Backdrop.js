bf65ef1f7a947e338172a8bca311495238c0feac
Chris Lopez <chrislopez28@gmail.com>
2021-05-20 16:35:16 -0700

    Updated gitignore

--- built/components/UI/Backdrop/Backdrop.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var Backdrop_module_css_1 = require("./Backdrop.module.css");
-var backdrop = function (props) { return (props.show ? <div className={Backdrop_module_css_1.default.Backdrop} onClick={props.click}></div> : null); };
-exports.default = backdrop;
